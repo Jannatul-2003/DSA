e5ecac5cdbc3d6938aaeada188f7fbe0cb34738b
Jannatul-2003 <jannatul19112003@gmail.com>
2024-01-31 23:49:21 +0600

    Added delFirst

--- DoubleLL.cpp
+++ DoubleLL.cpp
@@ -94,39 +94,69 @@ struct DoubleLL
         }
         return x;
     }
-    //insert value at intermediate position
+
+    // insert value at intermediate position
     void insert(char x, int position)
     {
-        node* newNode=new node();
-        newNode->data=x;
-        newNode->next=newNode->prev=NULL;
-        node* temp=head;
+        node *newNode = new node();
+        newNode->data = x;
+        newNode->next = newNode->prev = NULL;
+        node *temp = head;
         position--;
-        while(position!=1)
+        while (position != 1)
         {
-            temp=temp->next;
+            temp = temp->next;
             position--;
         }
-        newNode->prev=temp;
-        newNode->next=temp->next;
-        temp->next=newNode;
-        newNode->next->prev=newNode;
+        newNode->prev = temp;
+        newNode->next = temp->next;
+        temp->next = newNode;
+        newNode->next->prev = newNode;
     }
-
-    //reversing the list
+    // delete first occurance of value
+    int delFirst(char x)
+    {
+        if (!head)
+            return INT16_MIN;
+        if (head->data == x)
+        {
+            pop();
+            return 1;
+        }
+        int position = 1;
+        node *current = head;
+        while (current)
+        {
+            position++;
+            if (current->next->data == x)
+            {
+                node *tobefree = current->next;
+                current->next = current->next->next;
+                if (current->next)
+                    current->next->prev = current;
+                else
+                    tail = current;
+                free(tobefree);
+                return position;
+            }
+            current = current->next;
+        }
+        return INT16_MIN; // element not found
+    }
+    // reversing the list
     void reverse()
     {
-        if(!head || head==tail)
+        if (!head || head == tail)
             return;
-        node* current=head;
-        head=tail;//head tail interchange
-        while(current)
+        node *current = head;
+        head = tail;    // head tail interchange
+        tail = current; // to update tail
+        while (current)
         {
-            node* temp=current->next;//swap among prev next 
-            current->next=current->prev;
-            current->prev=temp;
-            tail=current;//to update tail
-            current=current->prev;//next has beome prev so..
+            node *temp = current->next; // swap among prev next
+            current->next = current->prev;
+            current->prev = temp;
+            current = current->prev; // next has beome prev so..
         }
     }
     // printlist
@@ -158,7 +188,7 @@ int main()
         stack->push(x);
     }
 
-    cout<<"Stack:"<<endl;
+    cout << "Stack:" << endl;
     stack->printList();
 
     cout << endl
@@ -171,28 +201,38 @@ int main()
     stack->pop_back();
     stack->printList();
     cout << endl;
-    
+
     char val;
     int pos;
-    cout<<"Insert:";
-    cin>>val;
-    cout<<"At intermediate position:";
-    cin>>pos;
-    stack->insert(val,pos);
-    cout<<"New stack:"<<endl;
+    cout << "Insert:";
+    cin >> val;
+    cout << "At intermediate position:";
+    cin >> pos;
+    stack->insert(val, pos);
+    cout << "New stack:" << endl;
     stack->printList();
     cout << endl;
 
-    cout<<"After reversal:"<<endl;
+    cout << "After reversal:" << endl;
     stack->reverse();
     stack->printList();
     cout << endl;
+
+    cout << "Delete first occurance of char:" << endl;
+    cin >> val;
+    i = stack->delFirst(val);
+    cout << "The stack:";
+    stack->printList();
+    cout << endl
+         << "The deleted char was at " << i << " position" << endl;
+
     // double ll as queue
     cout << endl
          << "Queue Implementation:" << endl
          << endl
          << "Enqueue five char:" << endl;
     DoubleLL *queue = new DoubleLL();
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
     i = 5;
     while (i--)
     {
@@ -200,32 +240,46 @@ int main()
         cin >> x;
         queue->enqueue(x);
     }
-    cout<<"Queue:"<<endl;
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
+    cout << "Queue:" << endl;
     queue->printList();
-    
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
+
     cout << endl
          << "After dequeueing:" << endl;
     queue->pop();
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
     queue->printList();
     cout << endl;
 
     cout << "After popping back:" << endl;
     queue->pop_back();
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
     queue->printList();
     cout << endl;
 
-    cout<<"Insert:";
-    cin>>val;
-    cout<<"At intermediate position:";
-    cin>>pos;
-    queue->insert(val,pos);
-    cout<<"New queue:"<<endl;
+    cout << "Insert:";
+    cin >> val;
+    cout << "At intermediate position:";
+    cin >> pos;
+    queue->insert(val, pos);
+    // cout << endl<< "head:" << queue->head << endl << "tail:" tail << endl;
+    cout << "New queue:" << endl;
     queue->printList();
     cout << endl;
-        
-    cout<<"After reversal:"<<endl;
+
+    cout << "After reversal:" << endl;
     queue->reverse();
+    // cout << endl<< "head:" << queue->head << endl<< "tail:" << queue->tail << endl;
     queue->printList();
     cout << endl;
+
+    cout << "Delete first occurance of char:" << endl;
+    cin >> val;
+    i = queue->delFirst(val);
+    cout << "The queue:";
+    queue->printList();
+    cout << endl
+         << "The deleted char was at " << i << " position" << endl;
     return 0;
 }
